ac0a978e8a0415f955487cb946c31ec399913dc2
籽沐 <vivian.xul@alibaba-inc.com>
2016-03-23 20:50:48 +0800

    Initial Commit

---
+++ log/log.h
@@ -0,0 +1,19 @@
+//
+//  log.h
+//  log
+//
+//  Created by Lynne on 16/3/23.
+//  Copyright © 2016年 Lynne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for log.
+FOUNDATION_EXPORT double logVersionNumber;
+
+//! Project version string for log.
+FOUNDATION_EXPORT const unsigned char logVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <log/PublicHeader.h>
+
+
