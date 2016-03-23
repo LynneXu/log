08ee5a2dc23a66c27401a1f03b5f4fe9b52291a4
籽沐 <vivian.xul@alibaba-inc.com>
2016-03-23 21:15:54 +0800

    rename

--- log/log.swift
+++ log/log.swift
@@ -5,5 +5,11 @@
 //  Created by Lynne on 16/3/23.
 //  Copyright © 2016年 Lynne. All rights reserved.
 //
-
 import Foundation
+
+public class Logger {
+    func log(parameters: String) {
+        
+        print("hello world!")
+    }
+}
